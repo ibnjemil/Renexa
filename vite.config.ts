eb004cf8b3157cb2c26fca0b4b8bce6a347b6932
ibnjemil <ibnjemil320@gmail.com>
2026-04-19 15:31:10 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    allowedHosts: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
   },
 });
