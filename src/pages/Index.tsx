e49784422cd32d7145ecf3ddc48e3ac34aaa64ef
ibnjemil <ibnjemil320@gmail.com>
2026-04-19 17:18:35 +0300

    Delete src/pages/Index.tsx

--- src/pages/Index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { WelcomeHero } from '../components/Home/WelcomeHero';
-
-export default function Index() {
-  return <WelcomeHero onGetStarted={() => window.location.href = '/spark'} />;
-}
